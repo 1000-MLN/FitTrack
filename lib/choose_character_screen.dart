6c8be241684aeb486edcc9dae0d8678de7e7d7f7
mc_lavrushka <mc.lavrushka@gmail.com>
2024-06-29 17:08:16 +0300

    add design to choose character and welcome screens

--- lib/choose_character_screen.dart
+++ lib/choose_character_screen.dart
@@ -4,22 +4,69 @@ class ChooseCharacterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/rabbit.png'),
-            SizedBox(height: 20),
-            Text('Choose your character', style: TextStyle(fontSize: 24)),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-              child: Text('Let\'s start!'),
-            ),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 10,
+                  child: Container(width: 500, height: 500, color: Colors.pink)),
+              const Padding(padding: EdgeInsets.all(16)),
+              const Spacer(flex: 1),
+              Expanded(
+                flex: 6,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Spacer(flex: 1),
+                    Expanded(
+                      flex: 6,
+                      child: Column(
+                        children: [
+                          Container(
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(
+                                    255, 255, 213, 227),
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              child: InkWell(
+                                  borderRadius: BorderRadius.circular(20),
+                                  onTap: () {
+                                     Navigator.pushNamed(context, '/home');
+                                    
+                                  },
+                                  child: Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 6),
+                                      child: Center(
+                                          child: Text(
+                                      'Let\'s start!',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleLarge,
+                                      )),
+                                    ),
+                                  )),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Spacer(flex: 1),
+                  ],
+                ),
+              ),
+              
+            ],
+          ),
         ),
-      ),
+      
     );
   }
 }
--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,23 +1,134 @@
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  final nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/hamster.png'),
-            SizedBox(height: 20),
-            Text('Hi!', style: TextStyle(fontSize: 24)),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/chooseCharacter');
-              },
-              child: Text('Start your journey!'),
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                  flex: 14,
+                  child:
+                      Container(width: 500, height: 500, color: Colors.pink)),
+              const Padding(padding: EdgeInsets.all(16)),
+              const Spacer(flex: 1),
+              Expanded(
+                flex: 6,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(flex: 1),
+                    Expanded(
+                      flex: 6,
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 25, vertical: 8),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                              ),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: TextField(
+                              expands: false,
+                              textAlign: TextAlign.center,
+                              textAlignVertical: TextAlignVertical.center,
+                              controller: nameController,
+                              style: Theme.of(context).textTheme.titleLarge,
+                              decoration: InputDecoration(
+                                isCollapsed: true,
+                                hintText: "Enter your name",
+                                hintStyle:
+                                    Theme.of(context).textTheme.titleLarge,
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          const Padding(padding: EdgeInsets.all(8)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: Ink(
+                                  decoration: BoxDecoration(
+                                    color: const Color.fromARGB(
+                                        255, 255, 213, 227),
+                                    borderRadius: BorderRadius.circular(20),
+                                    border: Border.all(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  child: InkWell(
+                                      borderRadius: BorderRadius.circular(20),
+                                      onTap: () {
+                                        if (nameController.text.isNotEmpty) {
+                                          //TODO:add here provider to remember name
+                                          Navigator.pushNamed(
+                                              context, '/chooseCharacter');
+                                        } else {
+                                          showDialog(
+                                              context: context,
+                                              builder: (context) => AlertDialog(
+                                                    title: const Align(
+                                                        alignment:
+                                                            Alignment.center,
+                                                        child: Text("Error")),
+                                                    content: const Row(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment
+                                                              .center,
+                                                      children: [
+                                                        Text(
+                                                            "Please enter a name"),
+                                                      ],
+                                                    ),
+                                                    actions: [
+                                                      TextButton(
+                                                          onPressed: () {
+                                                            Navigator.pop(
+                                                                context);
+                                                          },
+                                                          child: const Align(
+                                                            alignment: Alignment
+                                                                .center,
+                                                            child: Text("Ok"),
+                                                          ))
+                                                    ],
+                                                  ));
+                                        }
+                                      },
+                                      child: Expanded(
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 6),
+                                          child: Center(
+                                              child: Text(
+                                            "Continue",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleLarge,
+                                          )),
+                                        ),
+                                      )),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(flex: 1),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
